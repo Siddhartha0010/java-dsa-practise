d65ab132fc373a122d668aa746acb385469e540b
Siddhartha <guttapallivsiddhartha23@gmail.com>
2025-09-02 22:08:55 +0530

    Day 3- Completed 1D arrays including complex problems like rainwater,stockbuy problems

---
+++ arrays/buyandsellstock.java
@@ -0,0 +1,26 @@
+//Write a prog to find the best price to 
+import java.util.*;
+public class buyandsellstock{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int size = sc.nextInt();
+        int arr[] = new int[size];
+        for(int i=0;i<size;i++){
+            arr[i] = sc.nextInt();
+        }
+        int buyprice = Integer.MAX_VALUE;
+        int maxprofit = 0;
+        for(int i =0;i<size;i++){
+            if(buyprice<arr[i]){
+                int profit = arr[i] - buyprice;
+                maxprofit = Math.max(maxprofit,profit);
+            }
+            else{
+                buyprice = arr[i];
+            }
+        }
+        System.out.print("Max profit can be obtained is:"+maxprofit);
+
+    }
+
+}
---
+++ searchndsorting/bubblesort.java
@@ -0,0 +1,26 @@
+import java.util.*;
+public class bubblesort{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int arr[] = {0,6,1,-5,-10};
+        for(int i=0;i<arr.length-1;i++){
+            int count = 0;    // we are using count to check if the array is already sorted, then no swaps will happen in the first pass. 
+            for(int j=0;j<arr.length-1-i;j++){
+                if(arr[j]>arr[j+1]){
+                    int temp = arr[j];
+                    arr[j] = arr[j+1];
+                    arr[j+1] = temp;
+                    count++;
+                }
+                if(count == 0){
+                    break;                 // so here the TIME COMPLEXITY is --> O(n)    (without this count its O(n^2))
+                }
+                
+            }
+
+        }
+        for(int i=0;i<arr.length;i++){
+            System.out.print(arr[i]+" ");
+        }
+    }
+}
